d45a2a62b40cbda08caaece26e6eb8468d1f753c
rbsrafa <rbsrafa@gmail.com>
2018-09-15 09:30:28 +0100

    Commented main class

--- src/main/java/com/example/jpaonetoonerelationship/JpaOneToOneRelationshipApplication.java
+++ src/main/java/com/example/jpaonetoonerelationship/JpaOneToOneRelationshipApplication.java
@@ -24,24 +24,29 @@ public class JpaOneToOneRelationshipApplication implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        
+        // Clean all data from tables
         this.freePages.deleteAllInBatch();
         this.users.deleteAllInBatch();
         
+        // Create two users
         User user1 = new User("John", "Smith", "johnsmith@email.com", "john123");
         User user2 = new User("Linda", "Smith", "lindasmith@email.com", "linda123");
        
+        // Create two free pages
         FreePage freePage1 = new FreePage("john-project", "https://www.johnsite.com");
         FreePage freePage2 = new FreePage("linda-project", "https://www.lindasite.com");
         
+        // Set the free page to the respective user
         user1.setFreePage(freePage1);
         user2.setFreePage(freePage2);
         
+        // Set the user for the respective page
         freePage1.setUser(user1);
         freePage2.setUser(user2);
         
-        users.save(user1);
-        users.save(user2);
+        // Persist data to database
+        this.users.save(user1);
+        this.users.save(user2);
  
     }
 }
